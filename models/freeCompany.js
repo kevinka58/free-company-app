3f0ff863f5731a67b4c835a73448321b5233526c
Kevin Ayala <kevinayala6969@gmail.com>
2021-07-30 14:25:47 -0700

    adds freeCompany model and comment model

---
+++ models/freeCompany.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const freeCompanySchema = new Schema(
+    {
+        companyName: {
+            type: String,
+            required: true,
+        },
+        companyTag: {
+            type: String,
+            required: true,
+            maxLength: 3,
+        },
+        serverName: {
+            type: String,
+            required: true,
+            enum: [
+                "Adamantoise", "Cactuar", "Faerie", "Gilgamesh", "Jenova", "Midgardsormr", "Sargatanas", "Siren",
+                "Balmung", "Brynhildr", "Coeurl", "Diabolos", "Goblin", "Malboro", "Mateus", "Zalera",
+                "Behemoth", "Excalibur", "Exodus", "Famfrit", "Hyperion", "Lamia", "Leviathan", "Ultros",
+            ]
+        },
+        grandCompany: {
+            type: String,
+            required: true,
+            enum: [
+             "The Immortal Flames",
+             "The Maelstrom",
+             "The Order of the Twin Adder"
+            ]
+        },
+        rank: {
+            type: Number,
+            required: true,
+        },
+        companyPop: {
+            type: Number,
+            required: true
+        },
+        comment: [commentSchema],
+    },
+    {
+        timestamps: true
+    })
+
+const commentSchema = new Schema(
+    {
+        content: {
+            type: String,
+            required: true,
+        },
+        characterName: {
+            type: String,
+            required: true,
+        },
+        charLevel: {
+            type: Number,
+            required: true,
+        },
+        prefRole: {
+            type: String,
+            enum: [
+                "Tank",
+                "Healer",
+                "Damage (DPS)",
+            ],
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+    )
+
+module.exports = mongoose.model('FreeCompany', freeCompanySchema);
--- routes/api/users.js
+++ routes/api/users.js
@@ -3,12 +3,9 @@ const router = express.Router();
 const usersCtrl = require('../../controllers/api/users');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
-// GET /api/users/check-token
-router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
-// POST /api/users
 router.post('/', usersCtrl.create);
-// POST /api/users/login
 router.post('/login', usersCtrl.login);
+router.get('/check-token', ensureLoggedIn, usersCtrl.checkToken);
 
 
 module.exports = router;
