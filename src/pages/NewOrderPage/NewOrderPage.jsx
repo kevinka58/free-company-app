926d339da4d8b9b4d947eecb95a35fa24536fb09
Kevin Ayala <kevinayala6969@gmail.com>
2021-07-30 13:37:20 -0700

    first commit

---
+++ src/pages/NewOrderPage/NewOrderPage.jsx
@@ -0,0 +1,89 @@
+import { useState, useEffect, useRef } from 'react';
+import * as itemsAPI from '../../utilities/items-api';
+import * as ordersAPI from '../../utilities/orders-api';
+// Add the component imports
+import './NewOrderPage.css';
+import { Link, useHistory } from 'react-router-dom';
+import Logo from '../../components/Logo/Logo';
+import MenuList from '../../components/MenuList/MenuList';
+import CategoryList from '../../components/CategoryList/CategoryList';
+import OrderDetail from '../../components/OrderDetail/OrderDetail';
+import UserLogOut from '../../components/UserLogOut/UserLogOut';
+
+export default function NewOrderPage({ user, setUser }) {
+	const [menuItems, setMenuItems] = useState([]);
+	// Add state to track the "active" category
+	const [activeCat, setActiveCat] = useState('');
+	const [cart, setCart] = useState(null);
+
+	// Create and initialize the ref to an empty array
+	const categoriesRef = useRef([]);
+
+	// Use history object to change routes programmatically
+	const history = useHistory();
+
+	useEffect(() => {
+		async function getItems() {
+			const items = await itemsAPI.getAll();
+			categoriesRef.current = items.reduce((cats, item) => {
+				const cat = item.category.name;
+				return cats.includes(cat) ? cats : [...cats, cat];
+			}, []);
+			setMenuItems(items);
+			// Add this line to initialize the active category
+			setActiveCat(items[0].category.name);
+		}
+		getItems();
+
+		// Load cart (a cart is the unpaid order for the logged in user)
+		async function getCart() {
+			const cart = await ordersAPI.getCart();
+			setCart(cart);
+		}
+		getCart();
+	}, []);
+
+	/*--- Event Handlers --- */
+	async function handleAddToOrder(itemId) {
+		const cart = await ordersAPI.addItemToCart(itemId);
+		setCart(cart);
+	}
+
+	async function handleChangeQty(itemId, newQty) {
+		const cart = await ordersAPI.setItemQtyInCart(itemId, newQty);
+		setCart(cart);
+	}
+
+	async function handleCheckout() {
+		await ordersAPI.checkout();
+		history.push('/orders');
+	}
+
+	return (
+		<main className='NewOrderPage'>
+			<aside>
+				<Logo />
+				<CategoryList
+					categories={categoriesRef.current}
+					activeCat={activeCat}
+					setActiveCat={setActiveCat}
+				/>
+				<Link to='/orders' className='button btn-sm'>
+					PREVIOUS ORDERS
+				</Link>
+				<UserLogOut user={user} setUser={setUser} />
+			</aside>
+			<MenuList
+				menuItems={menuItems.filter(
+					item => item.category.name === activeCat
+				)}
+				handleAddToOrder={handleAddToOrder}
+			/>
+			<OrderDetail
+				order={cart}
+				handleChangeQty={handleChangeQty}
+				handleCheckout={handleCheckout}
+			/>
+		</main>
+	);
+}
