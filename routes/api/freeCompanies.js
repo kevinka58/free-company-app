9d129d12c3a451a169d9329ba432ff9b4ca03a17
Kevin Ayala <kevinayala6969@gmail.com>
2021-08-05 17:35:27 -0700

    fixes ensureLoggedIn Issue

--- routes/api/freeCompanies.js
+++ routes/api/freeCompanies.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const freeCompaniesCtrl = require('../../controllers/api/freeCompanies');
 const ensureLoggedIn = require('../../config/ensureLoggedIn');
 
-router.get('/', ensureLoggedIn, freeCompaniesCtrl.index);
+router.get('/', freeCompaniesCtrl.index);
 router.get('/:id', ensureLoggedIn, freeCompaniesCtrl.show);
 router.post('/', ensureLoggedIn, freeCompaniesCtrl.create);
 router.put('/:id', ensureLoggedIn, freeCompaniesCtrl.update);
